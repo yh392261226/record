fff803c7b194a288b409fe82270de25a930b5a07
杨浩 <yh392261226@sina.com>
2015-12-30 17:30:51 +0800

    增加第18个例子

--- go_language/sources/18.go
+++ go_language/sources/18.go
@@ -1,11 +1,25 @@
 package main
+
 import (
-    "fmt"
+	"fmt"
 )
+
 /**
- * 
+ * 函数 及返回值
  *
  */
 func main() {
-    
+	res := plus(1, 2)
+	fmt.Println("1+2=", res)
+
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1+2+3=", res)
+}
+
+func plus(a int, b int) int {
+	return a + b
+}
+
+func plusPlus(a, b, c int) int {
+	return a + b + c
 }
