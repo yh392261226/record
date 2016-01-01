5361be953875daebafaf5f6939b9841c102bb173
杨浩 <yh392261226@sina.com>
2016-01-02 01:46:29 +0800

    错误信息

--- go_language/sources/22.go
+++ go_language/sources/22.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+    "errors"
 )
 /**
- *
+ * 错误
  *
  */
 func main() {
-
+    err := errors.New("这是错误信息")
+    if nil != err {
+        fmt.Println(err)
+    }
 }
