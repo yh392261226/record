f337f417ee2b337b7f425f25a0293caf69cd8328
杨浩 <yh392261226@gmail.com>
2016-02-27 18:49:03 +0800

    fix the iptables bug, add /var/log/messages check

--- jst_server_shell/iptables_ips/iptables_ip.sh
+++ jst_server_shell/iptables_ips/iptables_ip.sh
@@ -14,7 +14,7 @@ attc_max=1000 #限定攻击最大次数值 默认值1000
 recordlog=./monior_ip_attc.log
 curdate=$(date|awk '{print $2" "$3}') #按照日志格式获取当前天
 today_attc_ips=$(cat $logfile|grep "$(echo $curdate)"|grep 'Failed password for\|authentication failure'|awk -F'from|rhost=' '{print $2}'|awk '{print $1}'|sort| uniq -c)
-today_attc_ips2=$(cat /var/log/messages |grep 'preauth'|awk -F'from' '{print $2}'|awk '{print $1}'|sort | uniq -c|grep -v 'socket'|sed 's/://g')
+today_attc_ips2=$(cat /var/log/messages |grep 'Failed password for'|awk -F'from' '{print $2}'|awk '{print $1}'|sort | uniq -c)
 today_attc_ips=$today_attc_ips$today_attc_ips2
 ifblack=0 #勿动
 docounter=0 #计数器
