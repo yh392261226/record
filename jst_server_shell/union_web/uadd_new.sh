573084e02fe06a6634c10796c364ef360b534847
杨浩 <yh392261226@gmail.com>
2016-04-21 14:08:15 +0800

    add new uadd

---
+++ jst_server_shell/union_web/uadd_new.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+#####################################################
+#
+# 添加开发用户
+#     并自动给予开发者所需的所有东西
+#
+#####################################################
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+  echo "Add new develop user";
+  echo "Usage: $0 username [project (merp|soufeel ...)]";
+  exit 1
+fi
+if [ "" != "$2" ]; then
+  pmark=$2    #项目标识
+else
+  pmark=soufeel  #默认是soufeel的网站 magento代码
+fi
+newusername=$1
+
+###配置
+web_path=/www                                                          					#网站目录
+shell_name=/bin/bash                                                            #登录使用的shell
+shell_config_path=/etc/skel                                                     #shell配置文件地址
+group_id=500                                                                    #用户组ID
+group_name=www                                                                  #用户组名称
+private=774                                                                     #用户家目录权限值
+log_privalleg=777 																															#日志权限 方便用户使用
+log_path=/logs                                                          				#日志目录
+nginx_conf_path=/usr/local/nginx/conf/vhost                    									#nginx 配置文件位置
+nginx_conf_source_path=/data/jst_server_shell/new_developer/nginx_conf     			#nginx 源配置文件位置
+code_source=/data/jst_server_shell/new_developer/code_source/$pmark        			#程序代码文件位置
+nginx_bin=/usr/local/nginx/sbin/nginx                                 					#nginx 执行程序位置
+public_path=/data/public                                      									#共享目录
+server_host=192.168.0.11                                                        #服务器ip地址
+smbconf=/etc/samba/smb.conf                                                     #samba配置文件地址
+alias_ext=com                                                                   #创建别名的后缀名
+email_ext=company.com                                                           #git global的邮箱后缀
+
+###预检测begin
+if [ ! -d $code_source ]; then
+  echo "Source code does not exists, please check !!!";
+  exit 1;
+fi
+checkuname=$(cat /etc/passwd | sed -n "/^$newusername\:/p")
+if [ "" != "$checkuname" ]; then
+  echo "User $newusername already exists !!!";
+  exit 1;
+fi
+###预检测end
+
+###添加用户 1
+useradd -b $web_path/$newusername -d $web_path/$newusername -g $group_id -s $shell_name -m -k $shell_config_path $newusername
+if [ "$?" = "1" ]; then
+  echo "Add user error";
+  exit 1
+fi
+echo "1:add user $newusername ok!";
+
+###创建家目录下的htdocs网站根目录 2
+mkdir $web_path/$newusername/htdocs
+chown -R ${newusername}:${group_name} $web_path/$newusername/htdocs
+if [ "$?" = "0" ]; then
+  echo "2:create htdocs for $newusername ok!";
+fi
+
+###给权限 3
+chmod $private -R $web_path/$newusername
+if [ "$?" = "0" ]; then
+  echo "3:change the privallage of home dir ok!";
+fi
+
+###创建nginx的log日志目录及error.log文件 4
+#mkdir -p $log_path/${newusername}
+#if [ "$?" = "0" ]; then
+#  echo "4:create log path for $newusername ok!";
+#fi
+### 5
+touch $web_path/${newusername}/error.log
+touch $web_path/${newusername}/access.log
+chmod $log_privalleg $web_path/${newusername}/error.log $web_path/${newusername}/access.log
+if [ "$?" = "0" ]; then
+  echo "4:create error log file for $newusername ok!";
+fi
+
+###复制nginx配置文件并按照用户名修改 6
+cp -r $nginx_conf_source_path $nginx_conf_path/${newusername}.${alias_ext}
+#mv $nginx_conf_path/${1}.${alias_ext}/nginx_conf.conf $nginx_conf_path/${1}.${alias_ext}/${1}.${alias_ext}
+
+###替换nginx配置源文件中目录
+sed -i -e "/include\ common\/vhost\/nginx_conf\/config\/http.conf;/ c\\
+include\ common\/vhost\/${newusername}.${alias_ext}\/config\/http.conf;" $nginx_conf_path/${newusername}.${alias_ext}/nginx_conf.conf;
+
+if [ "$pmark" == "merp" ]; then
+	sed -i -e "/include\ common\/location\/magento.conf;/ c\\
+	include\ common\/location\/${pmark}.conf;" $nginx_conf_path/${newusername}.${alias_ext}/nginx_conf.conf;
+fi
+
+###生成配置中的别名、错误日志地址及网站根目录
+cat << EOF > $nginx_conf_path/${newusername}.${alias_ext}/config/http.conf
+server_name ${newusername}.${alias_ext} www.${newusername}.${alias_ext};
+index index.html index.htm index.php;
+access_log $web_path/${newusername}/access.log;
+error_log $web_path/${newusername}/error.log;
+root $web_path/${newusername}/htdocs/;
+EOF
+if [ "$?" = "0" ]; then
+  echo "5:Nginx config of $newusername ok!";
+fi
+
+###重启nginx 7
+$nginx_bin -t   #先验证配置文件是否可用
+if [ "$?" != "0" ]; then
+  echo "6:Nginx config error, Please check it!!!";
+  exit 1
+fi
+#service nginx reaload > /dev/null &
+service nginx restart > /dev/null
+if [ "$?" = "0" ]; then
+  echo "6:restart nginx ok!";
+fi
+
+###将网站代码复制到家目录的htdocs根里 8
+cp -r $code_source/* $web_path/$newusername/htdocs/
+
+### 把一些软连接给连进来 比如media目录9
+ln -s $public_path $web_path/$newusername/htdocs/media
+
+if [ "$?" = "0" ]; then
+  echo "7:copy the code for $newusername ok!";
+fi
+### 复制完程序代码后 再赋一次权限 10
+chown -R ${newusername}:${group_name} $web_path/$newusername/htdocs/
+chmod -R 777 $web_path/$newusername/htdocs/
+
+###添加samba配置 11
+cat << EOF >> $smbconf
+[$newusername]
+path = /data/wwwroot/$newusername
+writable = no
+valid users = $newusername
+write list = $newusername
+read list = $newusername
+
+EOF
+###重启samba服务
+service smb restart > /dev/null &
+if [ "$?" = "0" ]; then
+  echo "8:restart samba ok!"
+fi
+
+###设置密码 12
+passwd $newusername
+smbpasswd -a $newusername
+if [ "$?" = "0" ]; then
+  echo "9:set password of $newusername ok!";
+fi
+
+###生成ssh的id_rsa
+ssh-keygen -t rsa -b 4096 -C "${newusername}@${email_ext}"
+if [ "$?" = "0" ]; then
+  echo "10:set ssh key of $newusername ok!";
+fi
+
+###生成一个新用户的git配置文件
+cat << EOF >> $web_path/$newusername/.gitconfig
+[user]
+  name = $newusername
+  email = ${newusername}@${email_ext}
+EOF
+if [ "$?" = "0" ]; then
+  echo "11:create git config global for $newusername ok!";
+fi
+
+### 13
+echo "All done!!!";
+echo "Please record the messages below";
+echo "*************************************************************"
+#echo "The username of the server for shell"
+echo "username: ${newusername}"
+#echo "The home folder of user ${newusername}"
+echo "home: $web_path/${newusername}"
+#echo "Put this into your local hosts file, Windows:C:/windows/system32/drivers/etc/hosts"
+echo "host: $server_host ${newusername}.${alias_ext} www.${newusername}.${alias_ext}"
+#echo "The nginx config folder";
+echo "nginx: $nginx_conf_path/${newusername}.${alias_ext}"
+#echo "The webroot folder of ${newusername}"
+echo "webroot: $web_path/${newusername}/htdocs"
+#echo "The nginx error log"
+echo "errorlog: $log_path/${newusername}/error.log"
+#echo "Samba info"
+echo "samba: \\\\$server_host\\$newusername"
+#echo "Id_rsa.pub info"
+echo  "id_rsa: $(cat $web_path/${newusername}/.ssh/id_rsa.pub)"
+echo "*************************************************************"
